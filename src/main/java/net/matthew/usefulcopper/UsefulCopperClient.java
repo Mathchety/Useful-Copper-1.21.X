5dbc52d04eda0d5a734ca7d3b7618f93723f1577
Matheus <matheusmarchetti1502@gmail.com>
2025-07-03 20:58:59 -0300

    Initial commit

---
+++ src/main/java/net/matthew/usefulcopper/UsefulCopperClient.java
@@ -0,0 +1,10 @@
+package net.matthew.usefulcopper;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UsefulCopperClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
